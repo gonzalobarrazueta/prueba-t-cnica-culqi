72776c204297d1b2f7c4b50f4d78cff0c5827c7b
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-04-16 18:04:14 -0500

    define post method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+const substrings = require('./prueba-1')
 const express = require('express')
-
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Server listening on port 3000");
-})
+});
+
+app.post('/substrings', (req, res) => {
+
+    const data = req.body;
+    const response = substrings.findSubstrings(data.s, data.words);
+
+    res.status(200).json({
+        response
+    });
+});
 
 app.listen(3000);
